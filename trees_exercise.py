78ad3d782f97514971d825cbce2225147fa78a4b
Alben <lichenguang123@hotmail.com>
2017-11-12 09:42:34 +0800

    finish trees algorithm

--- trees_exercise.py
+++ trees_exercise.py
@@ -4,45 +4,58 @@ import operator
 import pandas as pd
 
 
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing','flippers']
+    #change to discrete values
+    return dataSet, labels
 
-class trees_exercise(object):
-    def __init__(self, dataSet):
-        self.dataSet = dataSet
 
-    def calculate_shannon_entropy(self, data_frame):        
-        row_len = data_frame.shape[0]
+class trees_exercise_iteration(object):
+    def __init__(self):
+        pass
+
+    def calculate_shannon_entropy(self, data_frame):
+        data_frame_shape = data_frame.shape
         shannon_entropy = 0
 
+        if data_frame_shape[1] == 1:
+            return shannon_entropy
+        
         groupby_result = data_frame.groupby(data_frame.columns[-1]).count()
         for index in groupby_result.index:
-            prob = float(groupby_result.loc[index].values[0]) / row_len
+            prob = float(groupby_result.loc[index].values[0]) / data_frame_shape[0]
             shannon_entropy -= prob * log(prob, 2)
 
-        return shannon_entropy, data_frame
-        
+        return shannon_entropy
 
-    def choose_best_feature_split(self, dataSet):
-        base_shannon_entropy, data_frame = self.calculate_shannon_entropy(dataSet)
-        value_frame = data_frame
-        row_len = value_frame.shape[0]
+
+    def split_data_set(self, data_frame, feature, feature_value):
+        remained_data_frame = data_frame[data_frame[feature]==feature_value]
+        remained_data_frame = remained_data_frame.drop(feature, axis=1)
+        return remained_data_frame        
+
+
+    def choose_best_feature_split(self, data_frame):
+        base_shannon_entropy = self.calculate_shannon_entropy(data_frame)
+        row_len = data_frame.shape[0]
                 
         bestInfoGain = 0.0
         bestFeature = None
 
-        for feature in value_frame.columns:
-            if feature == value_frame.columns[-1]:
+        for feature in data_frame.columns:
+            if feature == data_frame.columns[-1]:
                 continue
 
             new_shannon_entropy = 0.0
-            for unique_feature_value in value_frame[feature].unique():             
-                remained_value_frame = self.split_data_set(value_frame, feature, unique_feature_value)
-                feature_shannon_entropy, remained_value_frame = self.calculate_shannon_entropy(remained_value_frame)
-
-                print('***remained***')
-                print(remained_value_frame)
-                print('***remained***')
-
-                prob = remained_value_frame.shape[0] / float(row_len)
+            for unique_feature_value in data_frame[feature].unique():             
+                remained_data_frame = self.split_data_set(data_frame, feature, unique_feature_value)
+                feature_shannon_entropy = self.calculate_shannon_entropy(remained_data_frame)
+                prob = remained_data_frame.shape[0] / float(row_len)
                 new_shannon_entropy += prob * feature_shannon_entropy
 
             sub_shannon_entropy = base_shannon_entropy - new_shannon_entropy
@@ -53,22 +66,21 @@ class trees_exercise(object):
         return bestFeature
 
 
-    def split_data_set(self, value_frame, feature, feature_value):
-        remained_value_frame = value_frame[value_frame[feature]==feature_value]
-        remained_value_frame = remained_value_frame.drop(feature, axis=1)
-        return remained_value_frame
+    def create_tree(self, data_frame):
+        data_frame_shape = data_frame.shape
+        label_frame = data_frame[data_frame.columns[-1]]
+        groupby_label_frame = label_frame.groupby(label_frame.values).count().sort_values(ascending=False)
+        '''
+            no     3
+            yes    2
+            Name: 2, dtype: int64
+        '''
 
+        if data_frame_shape[0] == groupby_label_frame[0]:
+            return groupby_label_frame.index[0]
 
-    def create_tree(self, data_frame, label_frame):        
-        row_len = data_frame.shape[0]
-        groupby_label_frame = label_frame.groupby(label_frame.values).count()
-        label_len = label_frame.shape[0]
-
-        if row_len == groupby_label_frame.shape[0]:
-            return label_frame.iloc[0]
-
-        if label_len == 1:
-            return None
+        if data_frame_shape[1] == 1:
+            return groupby_label_frame.index[0]
 
         bestFeature = self.choose_best_feature_split(data_frame)
         my_tree = {
@@ -76,21 +88,66 @@ class trees_exercise(object):
         }
 
         for best_fetaure_value in data_frame[bestFeature].unique():
-            remained_value_frame = self.split_data_set(data_frame, bestFeature, best_fetaure_value)
-            my_tree[bestFeature][best_fetaure_value] = self.create_tree(remained_value_frame, label_frame)
+            remained_data_frame = self.split_data_set(data_frame, bestFeature, best_fetaure_value)
 
-        print(my_tree)
+            '''
+            remained_data_frame = 
+                   1    2
+                0  1  yes
+                1  1  yes
+                2  0   no
+            or
+                   1   2
+                3  1  no
+                4  1  no            
+            '''
 
+            my_tree[bestFeature][best_fetaure_value] = self.create_tree(remained_data_frame)
+
+
+        return my_tree
+        
 
 if __name__ == '__main__':
     dataSet, labels = createDataSet()
+    '''
+    dataSet = 
+        [
+            [1, 1, 'yes'], 
+            [1, 1, 'yes'], 
+            [1, 0, 'no'], 
+            [0, 1, 'no'], 
+            [0, 1, 'no']
+        ]
+    '''
 
-    trees_exercise_obj = trees_exercise(dataSet)
+    # classList = ['yes', 'yes', 'no', 'no', 'no']
+
+    # if classList.count('yes') == len(classList):
+
+    # if len(dataSet[0]) == 1:
+    # len([1, 1, 'yes']) == 1
 
     data_frame = pd.DataFrame(dataSet)
 
-    shannon_entropy, data_frame = trees_exercise_obj.calculate_shannon_entropy(data_frame)
-    label_frame = data_frame[data_frame.columns[-1]]
-    value_frame = data_frame.iloc[:, 0:len(data_frame.columns) - 1]
-    trees_exercise_obj.create_tree(value_frame, label_frame)
-    # bestFeature = trees_exercise_obj.choose_best_feature_split(dataSet)
+    trees_exercise_obj = trees_exercise_iteration()
+    my_tree = trees_exercise_obj.create_tree(data_frame)
+    print(my_tree)
+    
+    '''
+    my_tree = 
+    {
+        0列: {
+            0: 'no',
+            1: {
+                1列: {
+                    0: 'no',
+                    1: 'yes'
+                }
+            }
+        }
+    }
+    '''
+
+
+
