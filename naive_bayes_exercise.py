b18f7450f4771e22442f3c65fb2c97d6082fea91
Alben <lichenguang123@hotmail.com>
2017-11-13 17:24:39 +0800

    optimize function code

--- naive_bayes_exercise.py
+++ naive_bayes_exercise.py
@@ -32,11 +32,52 @@ def set_of_words_to_vec(vocab_list, input_set):
 
 
 def trainNB0(train_matrix, train_category):
+    '''
+    train_matrix = 
+    [[0 0 1 0 0 0 1 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0 0 1 1 0 0 0 0 0 0 1]
+     [0 0 0 0 0 0 0 0 1 0 0 0 0 0 1 1 0 0 0 0 0 1 1 0 1 0 1 0 1 0 0 0]
+     [1 1 0 0 0 1 0 1 0 0 0 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 1]
+     [0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 1 0 0 0 0 0]
+     [0 0 0 0 0 0 0 0 0 1 0 0 1 0 0 1 0 0 0 0 1 1 0 0 0 1 0 0 0 1 1 1]
+     [0 0 0 0 1 0 0 0 0 0 0 0 0 1 0 0 1 0 0 1 0 0 0 0 1 0 1 0 0 0 0 0]]
+    '''
+
+    # train_category = [0 1 0 1 0 1]
+
     num_train_docs = len(train_matrix)
+    # num_train_docs = 6
     num_words = len(train_matrix[0])
-    p_abusive = np.ones(num_words)
-    print(p_abusive)
-    pass
+    # num_words = 32
+    p_abusive = sum(train_category) / float(num_train_docs)
+    # p_abusive = 3 / 6
+
+    p0_num = np.ones(num_words)
+    p1_num = np.ones(num_words)
+    p0_denom = 2.0
+    p1_denom = 2.0
+
+    for i in range(num_train_docs):
+        if train_category[i] == 1:
+            p1_num += train_matrix[i]
+            p1_denom += sum(train_matrix[i])
+        else:
+            p0_num += train_matrix[i]
+            p0_denom += sum(train_matrix[i])
+
+    p1_vect = np.log(p1_num / p1_denom)
+    p0_vect = np.log(p0_num / p0_denom)
+    # numpy.log = log e, value
+
+    return p0_vect, p1_vect, p_abusive
+
+
+def classify_NB(vec_to_classify, p0_vect, p1_vect, p_class1):
+    p1 = sum(vec_to_classify * p1_vect) + np.log(p_class1)
+    p0 = sum(vec_to_classify * p0_vect) + np.log(1.0 - p_class1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
 
 
 if __name__ == '__main__':
@@ -49,4 +90,9 @@ if __name__ == '__main__':
         return_vec = set_of_words_to_vec(vocab_list, posting)
         train_matrix.append(return_vec)
 
-    trainNB0(np.array(train_matrix), np.array(classVec))
+    p0_vect, p1_vect, p_abusive = trainNB0(np.array(train_matrix), np.array(classVec))
+
+    test_entry = ['love', 'my', 'dalmation']
+    return_vec = set_of_words_to_vec(vocab_list, test_entry)
+    classified_result = classify_NB(return_vec, p0_vect, p1_vect, p_abusive)
+    print(classified_result)
