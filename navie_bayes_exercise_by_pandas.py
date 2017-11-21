089a9dc6a2483cd83d26a755ed31b743a757780d
Alben <lichenguang123@hotmail.com>
2017-11-21 10:24:23 +0800

    add use pandas implement navie bayes

---
+++ navie_bayes_exercise_by_pandas.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import os
+
+
+def loadDataSet():
+    postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                 ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                 ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                 ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                 ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                 ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0, 1, 0, 1, 0, 1]    #1 is abusive, 0 not
+    return postingList, classVec
+
+
+def create_vocab_list(dataSet):
+    vocab_set = set([])
+    for document in dataSet:
+        vocab_set = vocab_set | set(document)
+    return list(vocab_set)
